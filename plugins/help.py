536a727f403a163e697a7016ceb697c29db3b7eb
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-06-20 22:32:55 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,6 +3,11 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
+	Lasiya2 = InlineKeyboardMarkup([
+		[InlineKeyboardButton("Youtube tutorial", url="https://www.youtube.com/channel/UCHyseVcfusXkOClpwja00yg")],
+		 ])
 	
-    helptxt = f"• Just Send Youtube Url \n • And i will give Method list  \n Youcan use  @Renamerv12bot & @Dprojbot"
-    await message.reply_text(helptxt)
+    helptxt = f"<b>♔English help</b>\n• Just Send your Youtube video url⛓ \n• And i will give Method list to select your choice😋\n\n<b>♔සිංහල උදව්</b>\n• කොපි කරගත් Youtube ලින්කුව එවන්න.\n• එවිට ලැබෙන ලැයිස්තුවෙන් අවශ්‍ය පමාණය හා මාදිලිය තෝරාදෙන්න.\n\n Also use our file renamer @Renamerv12bot"
+    thumbnail_url = "https://telegra.ph/file/a0c714ea372017109ff4c.jpg"
+	
+	await message.reply_photo(thumbnail_url, helptxt, reply_markup=Lasiya2)

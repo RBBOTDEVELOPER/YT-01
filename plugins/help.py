b74647e09cc99d5838f37d783424324ffeb58e12
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-06-21 01:09:38 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -6,8 +6,5 @@ async def start(client, message):
 	Lasiya2 = InlineKeyboardMarkup([
 		[InlineKeyboardButton("Youtube tutorial", url="https://telegra.ph/file/27fefe183e306ef919437.jpg")],
 		 ])
-	    HELP_TXT = "**♔English help**\n• Just Send your Youtube video url⛓ \n• And i will give Method list to select your choice😋\n\n**♔සිංහල උදව්**\n• කොපි කරගත් Youtube ලින්කුව එවන්න.\n• එවිට ලැබෙන ලැයිස්තුවෙන් අවශ්‍ය පමාණය හා මාදිලිය තෝරාදෙන්න.\n\n Also use our file renamer @Renamerv12bot"
-
-  	  thumbnail_url = "https://telegra.ph/file/a0c714ea372017109ff4c.jpg"
 	
-	await message.reply_photo(thumbnail_url,HELP_TXT, reply_markup=Lasiya2)
+	await message.send_photo("me","https://telegra.ph/file/a0c714ea372017109ff4c.jpg",  caption="**♔English help**\n• Just Send your Youtube video url⛓ \n• And i will give Method list to select your choice😋\n\n**♔සිංහල උදව්**\n• කොපි කරගත් Youtube ලින්කුව එවන්න.\n• එවිට ලැබෙන ලැයිස්තුවෙන් අවශ්‍ය පමාණය හා මාදිලිය තෝරාදෙන්න.\n\n Also use our file renamer @Renamerv12bot",reply_markup=Lasiya2)

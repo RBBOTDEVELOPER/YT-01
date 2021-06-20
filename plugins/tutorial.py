9acef0f6cb8b4e15747b827564c28afe6293785f
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-06-21 01:28:48 +0530

    Update tutorial.py

--- plugins/tutorial.py
+++ plugins/tutorial.py
@@ -1,9 +1,7 @@
 from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
 
-
-@Client.on_message(Filters.command(["tute"]),
+@Client.on_message(Filters.command(["tute"]))
 async def start(client, message):
-
     await message.reply_text("To make a YouTube downloader pess ⚜️ Go ⚜️ button to go youtube orexpand Instant viwe to watch video directly. 🥳",
                              reply_markup=InlineKeyboardMarkup(
         [
